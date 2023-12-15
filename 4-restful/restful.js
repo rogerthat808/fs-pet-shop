6dd42fbd5e44eb1dfc6bac4d0cf18e2fa5295f56
Rogelio <roger.cruz808@gmail.com>
2023-12-15 12:47:37 -0800

    finished

--- 4-restful/restful.js
+++ 4-restful/restful.js
@@ -1,5 +1,16 @@
 import express from 'express';
 import petsData from '../pets.json' assert {type: 'json'};
+import fs from 'fs'
+
+////////////////// database
+import pg from 'pg';
+
+let client = new pg.Client({
+    database: "petsdb"
+})
+
+await client.connect()
+
 const app = express();
 
 app.use(express.json());
@@ -8,20 +19,57 @@ app.use(logger)
 
 // CRUD 
 // app 
-app.get('/pets', (req, res) => {
-    res.send(petsData)
-})
+app.get('/pets', async (req, res) => {
+    fs.readFile('../pets.json', 'utf-8', async (error, data) => {
+        if (error) {
+            console.error(error);
+            res.status(500).send('Error loading data');
+        } else {
+            const petsFromJson = JSON.parse(data); // Assuming petsData was loaded from JSON
+            for (let i = 0; i < petsFromJson.length; i++) {
+                const pet = petsFromJson[i];
+
+                const checkQuery = 'SELECT * FROM pets WHERE id = $1';
+                const checkResult = await client.query(checkQuery, [pet.id]);
+
+                if (checkResult.rows.length === 0) {
+                    // If the pet doesn't exist in the database, insert it
+                    const queryText = 'INSERT INTO pets (id, name, age, kind) VALUES ($1, $2, $3, $4)';
+                    const values = [pet.id, pet.name, pet.age, pet.kind];
+                    await client.query(queryText, values);
+                }
+            }
+            res.send(data);
+        }
+    });
+});
+
+
 // post
-app.post('/pets', (req, res) => {
-    let body = req.body
-    if (body.name == undefined || body.age == undefined || body.kind == undefined) {
-        let response = req.method + " " + JSON.stringify(req.body) + " " + req.url + " Bad Request";
-        res.status(400).send(response)
+app.post('/pets', async (req, res) => {
+    let body = req.body;
+
+    if (body.name === undefined || body.age === undefined || body.kind === undefined) {
+        let response = `${req.method} ${JSON.stringify(req.body)} ${req.url} Bad Request`;
+        res.status(400).send(response);
+        // if above statement is true then return nothing... break;
+        return;
     }
-    let newPet = req.body
-    petsData.push(newPet);
-    res.status(201).send(newPet)
-})  
+    // if if statement is false then try this 
+    try {
+        const queryText = 'INSERT INTO pets (name, age, kind) VALUES ($1, $2, $3)'; //1,2,3 are placeholders in sql
+        const values = [body.name, body.age, body.kind];
+
+        const result = await client.query(queryText, values);
+        let newPet = req.body;
+        petsData.push(newPet);
+        res.status(201).send(newPet);
+    } catch (error) {
+        console.error('Error executing query:', error);
+        res.status(500).send('Error inserting pet');
+    }
+});
+
 
 // get /pets/3
 app.get('/pets/3', (req, res) => {
@@ -34,7 +82,7 @@ app.get('/pets/3', (req, res) => {
 })
 
 // PATCH /pets/3
-app.patch('/pets/3', (req, res) => {
+app.patch('/pets/3', async (req, res) => {
     let thirdPet = petsData[2]
     let body = req.body;
     thirdPet.name = body.name
@@ -49,7 +97,7 @@ app.delete('/pets/3', (req, res) => {
     res.status(200).send(thirdPet)
 })
 
-/////////////////////// Bonus :
+// Bonus kinda?:
 
 app.get('/', (req, res) => {
     res.status(404).send('Not Found')
@@ -68,3 +116,6 @@ function logger(req, res, next) {
     console.log("Request path: " , req.url);
     next();
 }
+
+
+
