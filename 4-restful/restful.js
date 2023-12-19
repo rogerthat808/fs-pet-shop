95a09c42a4785a1df0352abcee56b3d0ff9e7f88
Rogelio <roger.cruz808@gmail.com>
2023-12-19 08:44:58 -0800

    Finsihed
    app is now writing to pets.json and the pets database.

--- 4-restful/restful.js
+++ 4-restful/restful.js
@@ -20,30 +20,30 @@ app.use(logger)
 // CRUD 
 // app 
 app.get('/pets', async (req, res) => {
-    fs.readFile('../pets.json', 'utf-8', async (error, data) => {
-        if (error) {
-            console.error(error);
-            res.status(500).send('Error loading data');
-        } else {
-            const petsFromJson = JSON.parse(data); // Assuming petsData was loaded from JSON
+    try {
+        const checkEmptyQuery = 'SELECT COUNT(*) FROM pets';
+        const checkResult = await client.query(checkEmptyQuery);
+        const tableIsEmpty = checkResult.rows[0].count === '0';
+
+        if (tableIsEmpty) {
+            const data = await fs.promises.readFile('../pets.json', 'utf-8');
+            const petsFromJson = JSON.parse(data);
+            const insertQuery = 'INSERT INTO pets (name, age, kind) VALUES ($1, $2, $3)';
+
             for (let i = 0; i < petsFromJson.length; i++) {
                 const pet = petsFromJson[i];
-
-                const checkQuery = 'SELECT * FROM pets WHERE id = $1';
-                const checkResult = await client.query(checkQuery, [pet.id]);
-
-                if (checkResult.rows.length === 0) {
-                    // If the pet doesn't exist in the database, insert it
-                    const queryText = 'INSERT INTO pets (id, name, age, kind) VALUES ($1, $2, $3, $4)';
-                    const values = [pet.id, pet.name, pet.age, pet.kind];
-                    await client.query(queryText, values);
-                }
+                const values = [pet.name, pet.age, pet.kind];
+                await client.query(insertQuery, values);
             }
             res.send(data);
+        } else {
+            res.send(petsData);
         }
-    });
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).send('Error processing data');
+    }
 });
-
 
 // post
 app.post('/pets', async (req, res) => {
@@ -52,17 +52,24 @@ app.post('/pets', async (req, res) => {
     if (body.name === undefined || body.age === undefined || body.kind === undefined) {
         let response = `${req.method} ${JSON.stringify(req.body)} ${req.url} Bad Request`;
         res.status(400).send(response);
-        // if above statement is true then return nothing... break;
+        // if above statement is true then return nothing.
         return;
     }
     // if if statement is false then try this 
     try {
+        let newPet = req.body;
         const queryText = 'INSERT INTO pets (name, age, kind) VALUES ($1, $2, $3)'; //1,2,3 are placeholders in sql
         const values = [body.name, body.age, body.kind];
-
         const result = await client.query(queryText, values);
-        let newPet = req.body;
+
+        let petsData = [];
+        if (fs.existsSync('../pets.json')) {
+            const data = fs.readFileSync('../pets.json', 'utf-8');
+            petsData = JSON.parse(data);
+        }
+
         petsData.push(newPet);
+        fs.writeFileSync('../pets.json', JSON.stringify(petsData, null, 2), 'utf-8');
         res.status(201).send(newPet);
     } catch (error) {
         console.error('Error executing query:', error);
@@ -116,6 +123,3 @@ function logger(req, res, next) {
     console.log("Request path: " , req.url);
     next();
 }
-
-
-
